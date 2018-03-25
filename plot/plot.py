f08acf95b88aa604b36f85065d6b3e555c5259e7
Tristan1900 <452787782@qq.com>
2018-03-25 18:42:37 -0400

    Wrong version last time, this is the refined plot

--- plot/plot.py
+++ plot/plot.py
@@ -3,6 +3,7 @@ from matplotlib import style
 import matplotlib.animation as animation
 import numpy as np
 import sys
+import optparse
 from sar import get_sar
 from smart import get_smart
 from hitratio import get_stat
@@ -10,13 +11,15 @@ from admission import get_admission
 
 class Draw:
 	def __init__(self):
-		style.use('ggplot')
+		self.style = style.use('ggplot')
 		self.fig = plt.figure(figsize=(10, 8))
 		self.ax1 = self.fig.add_subplot(2,2,1)
 		self.ax2 = self.fig.add_subplot(2,2,2)
 		self.ax3 = self.fig.add_subplot(2,2,3)
 		self.ax4 = self.fig.add_subplot(2,2,4)
+		self.fontsize = 8
 		self.smooth = 1
+		self.file=""
 
 	def compress_array(self, array):
 		array = np.nanmean(np.pad(array.astype(float), 
@@ -29,55 +32,69 @@ class Draw:
 		sample every 5 seconds, so a compress factor of 12 means 1 miniute
 		a compress factor of 240 means every 20 miniutes.
 		"""
-		sar_name = "../sar.dat"
-		stat_name = "../stat.dat"
-		smart_name = "../smart.dat"
-		admission_name = "../admission.dat"
-		# 
-		sar_data = get_sar(sar_name)
-		# print(len(sar_data))
-		sar_data = self.compress_array(sar_data)
-		# 
-		stat_data = get_stat(stat_name)
-		# print(len(stat_data))
-		stat_data = self.compress_array(stat_data)
-		# 
-		smart_data, total = get_smart(smart_name)
-		# print(len(smart_data))
-		# smart_data = compress_array(smart_data,12 )
-		# smart_data = smart_data[:20]
-		# row = len(smart_data) % 20
-		# smart_data = np.reshape(smart_data[:480*2],(48,-1))
-		#
-		admission_data = get_admission(admission_name)
-		admission_data = self.compress_array(admission_data)
-
+		sar_name = "../result4/"
+		stat_name = "../result4/"
+		smart_name = "../result4/"
+		admission_name = "../result4/"
 		self.ax1.clear()
 		self.ax2.clear()
 		self.ax3.clear()
 		self.ax4.clear()
+		self.plot_sar(sar_name)
+		self.plot_stat(stat_name)
+		self.plot_smart(smart_name)
+		self.plot_admission(admission_name)
 
-		self.ax1.set_title("Disk Utilization")
-		self.ax1.set_xlabel("time/min")
-		self.ax1.set_ylabel("utilization/percent")
-
-		self.ax2.set_title("hitratio")
-		self.ax2.set_xlabel("time/min")
-		self.ax2.set_ylabel("hitratio/percent")
-
-		self.ax3.set_title("SMART")
-		self.ax3.set_xlabel("time/20min")
-		self.ax3.set_ylabel("writeToDisk(MB)/min")
-
-		self.ax4.set_title("Admission")
-		self.ax4.set_xlabel("time/min")
-		self.ax4.set_ylabel("size(MB)/s")
-
+	def plot_sar(self, filename):
+		path = filename + "/sar.dat"
+		sar_data = get_sar(path)
+		sar_data = self.compress_array(sar_data)
+		self.ax1.set_title("Disk Utilization",fontsize=self.fontsize)
+		self.ax1.set_xlabel("time",fontsize=self.fontsize)
+		self.ax1.set_ylabel("utilization/percent",fontsize=self.fontsize)
+		self.ax1.tick_params(axis='both', which='major', labelsize=self.fontsize)
 		self.ax1.plot(sar_data)
+
+	def plot_stat(self, filename):
+		path = filename + "/stat.dat"
+		stat_data = get_stat(path)
+		stat_data = self.compress_array(stat_data)
+		self.ax2.set_title("hitratio",fontsize=self.fontsize)
+		self.ax2.set_xlabel("time",fontsize=self.fontsize)
+		self.ax2.set_ylabel("hitratio/percent",fontsize=self.fontsize)
+		self.ax2.tick_params(axis='both', which='major', labelsize=self.fontsize)
 		self.ax2.plot(stat_data)
+
+	def plot_smart(self, filename):
+		path = filename + "/smart.dat"
+		smart_data, total = get_smart(path)
+		row = len(smart_data) % 80
+		smart_data = np.reshape(smart_data[:-row],(80,-1))
+		self.ax3.set_title("SMART",fontsize=self.fontsize)
+		self.ax3.set_xlabel("time",fontsize=self.fontsize)
+		self.ax3.set_ylabel("writeToDisk(MB)/min",fontsize=self.fontsize)
+		self.ax3.tick_params(axis='both', which='major', labelsize=self.fontsize)
 		self.ax3.boxplot(smart_data)
-		# ax3.plot(smart_data[:20])
+
+	def plot_admission(self, filename):
+		path = filename + "/admission.dat"
+		admission_data = get_admission(path)
+		admission_data = self.compress_array(admission_data)
+		self.ax4.set_title("Admission",fontsize=self.fontsize)
+		self.ax4.set_xlabel("time",fontsize=self.fontsize)
+		self.ax4.set_ylabel("size(MB)/s",fontsize=self.fontsize)
+		self.ax4.tick_params(axis='both', which='major', labelsize=self.fontsize)
 		self.ax4.plot(admission_data)
+
+	def plot_all(self):
+		prefix = "/Users/Tristan/Desktop/research_code/"
+		for file in self.file:
+			self.plot_sar(prefix+file)
+			self.plot_smart(prefix+file)
+			self.plot_stat(prefix+file)
+			self.plot_admission(prefix+file)
+		plt.show()
+
 	def plot(self):
 		ani = animation.FuncAnimation(self.fig, self.animate, interval = 10000)
 		plt.show()
@@ -85,10 +102,22 @@ class Draw:
 
 
 def main(args):
+	# parse arguments
+	parser = optparse.OptionParser()
+	parser.add_option('-m','--minute', action="store", dest="minute", help="granularity", default=1)
+	parser.add_option('-b', '--box', action="store", dest="box", help="box granularity", default=1)
+	parser.add_option('-f', '--file', action="store", dest="file", help="file name", default="")
+
+	options, args = parser.parse_args()
+	minute = int(options.minute)*12
+	box = int(options.box)*12
+	file = options.file.split(",")
 	draw = Draw()
-	if len(args) > 1:
-		draw.smooth = int(args[1])
-	draw.plot()
+	draw.smooth=minute
+	draw.box=box
+	draw.file=file
+	draw.plot_all()
+	# draw.plot()
 
 if __name__ == '__main__':
 	main(sys.argv)
